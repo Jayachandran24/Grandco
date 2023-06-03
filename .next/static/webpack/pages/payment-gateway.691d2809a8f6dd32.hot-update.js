f6f238c0379234b24c53f2ad795f0ad641cf0389
GRDesigner <nithish@grinfotech.com>
2023-06-03 09:48:52 +0530

    first commit

---
+++ .next/static/webpack/pages/payment-gateway.691d2809a8f6dd32.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/payment-gateway",{
+
+/***/ "./src/data/project/pg-description.json":
+/*!**********************************************!*\
+  !*** ./src/data/project/pg-description.json ***!
+  \**********************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"description":"Save time, improve cash flow, and control processing costs with built-in productivity boosters. Customize buy buttons, payment pages and invoices with your brand to improve the customer experience.","features":[{"numb":"01","title":"Card on file","details":"Securely store customer profiles and card details for repeat, recurring and subscription payments."},{"numb":"02","title":"Remote billing and invoicing","details":"Email customers quotes and invoices and allow them to pay online with a simple click."},{"numb":"03","title":"Credit card surcharge","details":"Offset costs with our industry-compliant program that allows you to pass acceptance costs to customers."},{"numb":"04","title":"Level III processing","details":"Lower interchange costs for business and government transactions by entering line-item details from invoices."},{"numb":"05","title":"Product catalog","details":" Speed checkout and reduce staff keying errors by setting up a list of products and services including prices and descriptions."},{"numb":"06","title":"Reporting and management","details":"Easily generate standard or custom reports to view or export to accounting and management systems."},{"numb":"07","title":"Administrative toolst","details":"Add employees and assign roles based on access rights."}]}');
+
+/***/ })
+
+});
---
+++ .next/static/webpack/pages/products.3fb34b9f006a1ba3.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/products",{
+
+/***/ "./src/data/portfolio/classic/masonry.json":
+/*!*************************************************!*\
+  !*** ./src/data/portfolio/classic/masonry.json ***!
+  \*************************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"filters":[{"name":"All","operator":"*"},{"name":"Payment Terminals","operator":".brand"},{"name":"Point Of Sale Solutions","operator":".web"},{"name":"Promotions","operator":".graphic"}],"gallery":[{"filter":"graphic","image":"img/solution/talech.png","title":"Creativity Demand","type":"Photography"},{"filter":"web","image":"img/solution/poynt.png","title":"Through The Breaking","type":"Web design"},{"filter":"brand","image":"img/solution/payment.png","title":"Create With Creatives","type":"Branding"},{"filter":"graphic","image":"img/solution/billing.png","title":"Energies of Love","type":"graphic"},{"filter":"web","image":"img/solution/newland.png","title":"See It Yourself","type":"Design"},{"filter":"brand","image":"img/solution/billing.png","title":"Blast From The Past","type":"Branding"},{"filter":"web","image":"img/portfolio/1/6.jpg","title":"See It Yourself","type":"Design"},{"filter":"brand","image":"img/portfolio/1/8.jpg","title":"Blast From The Past","type":"Branding"}]}');
+
+/***/ })
+
+});
